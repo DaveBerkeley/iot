ca884e888abf5d84936e5d3cf79d778a8622bd35
Dave Berkeley <git@rotwang.co.uk>
2015-03-09 11:55:53 +0000

    added sleep.lua

---
+++ sleep.lua
@@ -0,0 +1,42 @@
+
+relay = 1
+key = 3
+on = false
+gpio.mode(relay, gpio.OUTPUT)
+
+function set(state)
+    on = state
+    if state == false then
+        gpio.write(relay, gpio.LOW)
+    else
+        gpio.write(relay, gpio.HIGH)
+    end
+end
+
+function on_msg(msg)
+    print(msg)
+    if msg == "on" then
+        set(true)
+    elseif msg == "off" then
+        set(false)
+    elseif msg == "toggle" then
+        set(not on)
+    end
+end
+
+-- Create a UDP listener
+s = net.createServer(net.UDP) 
+s:on("receive",function(conn, data) on_msg(data) end)
+s:listen(5000)
+
+set(false)
+
+-- gpio.mode(relay, gpio.INPUT)
+gpio.mode(relay, gpio.INT)
+gpio.trig(key, "down", function() print ("key") end)
+
+-- goto sleep in 10
+print('tmr.alarm(0, 10000, 0, function() print("none") end)')
+
+tmr.alarm(0, 10000, 0, function() node.dsleep(10000000) end)
+
