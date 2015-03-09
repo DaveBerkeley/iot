8672c3e5eb9244ab4ffd85aef150590d40f9b5a5
Dave Berkeley <git@rotwang.co.uk>
2015-03-09 19:16:47 +0000

    remove udp server

--- sleep.lua
+++ sleep.lua
@@ -1,41 +1,8 @@
 
 relay = 1
-key = 3
-on = false
 gpio.mode(relay, gpio.OUTPUT)
+gpio.write(relay, gpio.HIGH)
 
-function set(state)
-    on = state
-    if state == false then
-        gpio.write(relay, gpio.LOW)
-    else
-        gpio.write(relay, gpio.HIGH)
-    end
-end
-
-function on_msg(msg)
-    print(msg)
-    if msg == "on" then
-        set(true)
-    elseif msg == "off" then
-        set(false)
-    elseif msg == "toggle" then
-        set(not on)
-    end
-end
-
--- Create a UDP listener
-s = net.createServer(net.UDP) 
-s:on("receive",function(conn, data) on_msg(data) end)
-s:listen(5000)
-
--- set(false)
-set(true)
-
--- gpio.mode(relay, gpio.INPUT)
--- gpio.trig(key, "down", function() print ("key") end)
-
--- goto sleep in 10
 print('tmr.alarm(0, 1000, 0, function() print("none") end)')
 
 tmr.alarm(0, 10000, 0, function() node.dsleep(10000000) end)
