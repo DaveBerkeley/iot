14b18a3ad86e445e51724bd198f8ceac8238c15d
Dave Berkeley <git@rotwang.co.uk>
2015-03-11 08:53:32 +0000

    removed relay from pir

--- pir.lua
+++ pir.lua
@@ -1,17 +1,4 @@
 
-relay = 1
-on = false
-gpio.mode(relay, gpio.OUTPUT)
-
-function set_relay(state)
-    on = state
-    if state == false then
-        gpio.write(relay, gpio.LOW)
-    else
-        gpio.write(relay, gpio.HIGH)
-    end
-end
-
 function tx(req)
     conn=net.createConnection(net.TCP, false)
     conn:on("receive", function(conn, pl) print(pl) end)
@@ -19,11 +6,7 @@ function tx(req)
     conn:send("GET /" .. req .. " HTTP/1.1\r\nHost: esp8266\r\n\r\n")
 end
 
-set_relay(false)
-
 function on_pir()
-    set_relay(true)
-    tmr.alarm(0, 2000, function() set_relay(false) end)
     tx("wiki/iot.cgp?pir=1")
 end
 
