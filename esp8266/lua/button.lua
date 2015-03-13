8020eac0d680641d8fb6f1c08bed0161acf78600
Dave Berkeley <git@rotwang.co.uk>
2015-03-13 15:35:44 +0000

    debounce key switches

--- esp8266/lua/button.lua
+++ esp8266/lua/button.lua
@@ -2,9 +2,16 @@
 key = 3     -- GPIO03
 gpio.mode(key, gpio.INPUT)
 
-function on_key()
-    tx("wiki/iot.cgp?key=1")
+function key_enable(handler)
+    gpio.trig(key, "down", handler)
 end
 
-gpio.trig(key, "down", on_key)
+function on_key()
+    -- implement debounce by ignoring presses for a period
+    key_enable(function() end)
+    tx("wiki/iot.cgp?key=1")
+    tmr.alarm(1, 250, 0, function() key_enable(on_key) end)
+end
+
+key_enable(on_key)
 
