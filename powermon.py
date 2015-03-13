63cb64b1640b834048d6aa47d61f5ef7c9ba525a
Dave Berkeley <git@rotwang.co.uk>
2015-03-13 20:47:35 +0000

    wip powermon

--- powermon.py
+++ powermon.py
@@ -8,22 +8,32 @@ from broker import Broker
 #
 #
 
-broker = Broker("myid", "mosquitto")
-
 class Handler:
 
     def __init__(self):
         self.history = []
+        self.size = 3
 
     def on_power(self, msg):
         data = json.loads(msg.payload)
         power = data["power"]
-        self.history = [ power, ] + self.history[:2]
-        if len(self.history) < 3:
+        self.process(power)
+
+    def process(self, power):
+        self.history = [ power, ] + self.history[:self.size-1]
+        if len(self.history) < self.size:
             return
         diff = self.history[0] - self.history[-1]
         av = sum(self.history) / len(self.history)
-        print int(av), [ int(x-av) for x in self.history ]
+        print int(av), 
+        print [ int(x-av) for x in self.history ], 
+        print [ int(self.history[0]), int(self.history[-1]) ],
+        print int(self.history[0]) - int(self.history[-1])
+
+#
+#
+
+broker = Broker("myid", "mosquitto")
 
 handler = Handler()
 
