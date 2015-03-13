7484d0bada090b59ff3d5fed4c107ee9be23aba9
Dave Berkeley <git@rotwang.co.uk>
2015-03-13 19:56:57 +0000

    powermon wip

--- powermon.py
+++ powermon.py
@@ -10,11 +10,24 @@ from broker import Broker
 
 broker = Broker("myid", "mosquitto")
 
-def on_power(msg):
-    data = json.loads(msg.payload)
-    print data["power"]
+class Handler:
 
-broker.subscribe("home/power", on_power)
+    def __init__(self):
+        self.history = []
+
+    def on_power(self, msg):
+        data = json.loads(msg.payload)
+        power = data["power"]
+        self.history = [ power, ] + self.history[:2]
+        if len(self.history) < 3:
+            return
+        diff = self.history[0] - self.history[-1]
+        av = sum(self.history) / len(self.history)
+        print int(av), [ int(x-av) for x in self.history ]
+
+handler = Handler()
+
+broker.subscribe("home/power", handler.on_power)
 
 broker.start()
 
