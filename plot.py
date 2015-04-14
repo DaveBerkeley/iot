38751f237b5487386bb7ccdb9d19ee2a08b51565
Dave Berkeley <git@rotwang.co.uk>
2015-04-14 12:09:15 +0100

    add start/end times to plot

--- plot.py
+++ plot.py
@@ -5,11 +5,23 @@ import os
 import datetime
 import json
 
+def hm2s(text):
+    h, m = text.split(":")
+    h, m = [ int(x) for x in [ h, m ] ]
+    return (m*60) + (h*60*60)
+
+
 base = "/usr/local/data/iot"
 
 day = sys.argv[1]
 subtopic = sys.argv[2]
 field = sys.argv[3]
+
+start, end = None, None
+if len(sys.argv) > 4:
+    start = hm2s(sys.argv[4])
+    if len(sys.argv) > 5:
+        end = hm2s(sys.argv[5])
 
 path = os.path.join(base, day + ".log")
 
@@ -40,8 +52,16 @@ gnuplot = [
     "set ylabel 'time'",
     "set xtics rotate by -45",
     "set key off",
-    "plot '%s' using 1:2 with lines" % opath,
+    "plot '%s' using 1:2 with lines smooth bezier" % opath,
 ]
+
+if start:
+    if end:
+        gnuplot.insert(0, "set xrange[%s:%s]" % (start, end))
+    else:
+        gnuplot.insert(0, "set xrange[%s:]" % start)
+elif end:
+    gnuplot.insert(0, "set xrange[:%s]" % end)
 
 for line in gnuplot:
     print line
