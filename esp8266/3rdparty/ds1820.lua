9da5660c3d4e04f2e194ad980a472a57f7672326
Dave Berkeley <dave@rotwang.co.uk>
2015-05-14 09:52:27 +0100

    move pir/temp code out of ds1820 library

--- esp8266/3rdparty/ds1820.lua
+++ esp8266/3rdparty/ds1820.lua
@@ -60,11 +60,4 @@ function getTemp(temperature_pin)
     until(addr == nil)
 end
 
-function txTemp()
-    t = getTemp(temperature_pin)
-    tx("wiki/iot.cgp?temp=" .. (t / 10000.0))
-end
-
--- send data every X ms
-tmr.alarm(0, 60000, 1, function() txTemp() end )
 
---
+++ esp8266/lua/pir_temp.lua
@@ -0,0 +1,9 @@
+
+function txTemp()
+    t = getTemp(temperature_pin)
+    tx("wiki/iot.cgp?temp=" .. (t / 10000.0))
+end
+
+-- send data every X ms
+tmr.alarm(0, 60000, 1, function() txTemp() end )
+
