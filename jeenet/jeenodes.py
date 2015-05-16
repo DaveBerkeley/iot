c5df2bacabc6b1a394e636606af579b9410602c6
Dave Berkeley <dave@rotwang.co.uk>
2015-05-16 13:46:42 +0100

    added humidity sensor to jeenet

--- jeenet/jeenodes.py
+++ jeenet/jeenodes.py
@@ -27,10 +27,50 @@ class TestDev(PirSensor):
         return None
 
 #
+#
+
+class HumidityDev(JeeNodeDev):
+
+    def __init__(self, *args, **kwargs):
+        JeeNodeDev.__init__(self, *args, **kwargs)
+
+    def to_info(self, data):
+        rx_fields = [ 
+            (1<<1, "temp", "<H"),
+            (1<<2, "humidity", "<H"), 
+            (self.text_flag, "text", "p"),
+        ]
+
+        msg_id, flags, info = message_info(data, self.fmt_header, rx_fields)
+
+        if not info.get("temp") is None:
+            info["temp"] /= 100.0
+        if not info.get("humidity") is None:
+            info["humidity"] /= 100.0
+
+        # ack is now handled by the radio board
+        #if flags & self.ack_flag:
+        #    self.hello(0, msg_id=msg_id)
+        return info
+
+    def set_mode(self, mode):
+        assert mode in [ 0, 1, 2 ]
+        log("set mode", mode)
+        fields = [ (1<<0, "<B", mode), ]
+        msg_id, raw = self.make_raw(self.ack_flag, fields)
+        self.tx_message(msg_id, raw, "set_mode", True)
+
+    def get_poll_period(self):
+        return None
+
+    api = JeeNodeDev.api + [ "set_mode" ]
+
+#
 #   Known device types
 
 known_devices = {
     "Test Device v1.0" : TestDev, # TODO
+    "Humidity Device v1.0" : HumidityDev, # TODO
     "Triac Control v1.0" : Triac,
     "PIR Device v1.0" : PirSensor,
 }
