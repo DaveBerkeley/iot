d0d86e487ec6864c6821347c3cfafecdb9ee5248
Dave Berkeley <dave@rotwang.co.uk>
2015-07-02 07:11:15 +0100

    add 5 and 10 minute filter to dust data

--- dust.py
+++ dust.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python -u
 
 import time
+import datetime
 import urllib2
+import optparse
 
 # http://pyserial.sourceforge.net/
 import serial
@@ -19,7 +21,7 @@ def log(*args):
 #
 
 def put(opts, data):
-    url = "http://%s/wiki/iot.cgp?" % opts.server
+    url = "http://%s/wiki/iot.cgp?" % opts.mqtt
     args = []
     for key, value in data.items():
         args.append("%s=%s" % (key, value))
@@ -41,26 +43,69 @@ def get_line(opts):
         time.sleep(5)
         return None
 
+#
+#   Filter
+
+class Filter:
+
+    def __init__(self, dt=datetime.timedelta(minutes=10)):
+        self.data = []
+        self.dt = dt
+
+    def add(self, value, dt):
+        self.data.append((dt, value))
+        while self.data:
+            t, v = self.data[0]
+            if t > (dt - self.dt):
+                break
+            del self.data[0]
+
+    def filtered(self, dt):
+        now, _ = self.data[-1]
+        total = 0.0
+        count = 0
+        for t, v in self.data:
+            if t < (now - dt):
+                continue
+            total += v
+            count += 1
+        return total / float(count)
+
+#
+#
+
 s = None
 
-class O:
-    pass
+if __name__ == "__main__":
 
-opts = O()
+    serial_dev = "/dev/dust"
 
-opts.dev = "/dev/nano_2-1_2"
-opts.server = "klatu"
+    p = optparse.OptionParser()
+    p.add_option("-d", "--dev", dest="dev", type="str", default=serial_dev)
+    p.add_option("-m", "--mqtt", dest="mqtt", default="mosquitto")
+    opts, args = p.parse_args()
 
-while True:
-    d = get_line(opts)
-    parts = d.strip().split(",")
-    print parts
-    conc = float(parts[2])
-    d = { 
-        "dust" : conc,
-        "subtopic" : "home/dust",
-    }
-    log(conc)
-    put(opts, d)
+    f = Filter()
+
+    while True:
+        d = get_line(opts)
+        parts = d.strip().split(",")
+        print parts
+        conc = float(parts[2])
+        
+        now = datetime.datetime.now()
+        f.add(conc, now)
+        
+        f_5 = f.filtered(datetime.timedelta(minutes=5))
+        f_10 = f.filtered(datetime.timedelta(minutes=10))
+
+        d = { 
+            "dust" : conc,
+            "dust_5" : f_5,
+            "dust_10" : f_10,
+            "subtopic" : "dust",
+        }
+        log(conc)
+        put(opts, d)
 
 # FIN
