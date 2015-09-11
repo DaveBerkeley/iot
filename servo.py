5009bd14032a2093b5c5f7667ca3be1d4016ebdd
Dave Berkeley <dave@rotwang.co.uk>
2015-09-11 13:17:57 +0100

    added servo code

---
+++ servo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import sys
+import time
+import json
+
+import broker
+
+import serial
+
+#
+#
+
+s = serial.Serial("/dev/nano_2-1_1", 9600)
+
+field = None
+topic = "uif/seek/1"
+#topic = "node/gas/sector"
+
+topic = "home/power"
+field = "power"
+
+def translate(p):
+    return int(p)
+
+def r_translate(p, minx, maxx):
+    d = maxx - minx
+    p -= minx
+    p /=  d
+    p *= 180
+    return int(p)
+
+if topic == "home/power":
+    def translate(p):
+        return r_translate(p, -2500.0, 2500.0)
+
+if topic == "node/gas/sector":
+    def translate(p):
+        return r_translate(p, 0.0, 64.0)
+
+#
+#
+
+def on_msg(x):
+    data = json.loads(x.payload)
+    if field:
+        data = data.get(field)
+        if data is None:
+            return
+    p = translate(data)
+    print data, p
+    s.write(str(p) + "\r")
+
+mqtt = broker.Broker("uif", server="mosquitto")
+mqtt.subscribe(topic, on_msg)
+
+mqtt.start()
+
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
+
+mqtt.stop()
+mqtt.join()
+
+# FIN
