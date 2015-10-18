225fae90e2f0067ebca9b911decf7a145621891d
Dave Berkeley <dave@rotwang.co.uk>
2015-10-18 17:30:51 +0100

    output to /dev/null on pi

--- jeenet/run.sh
+++ jeenet/run.sh
@@ -4,6 +4,6 @@
  
 # cp /tmp/jeenodes.log /tmp/jeenodes.log.old
 
-python -u ./jeenodes.py -d /dev/ttyUSB0 -i klatu -m mosquitto 2>/dev/null >/tmp/jeenodes.log &
+./jeenodes.py -d /dev/ttyUSB0 -i klatu -m mosquitto 2>/dev/null >/dev/null &
 
 # FIN
