75c3fff98e90c864724c45f151acb9501017a9f1
Dave Berkeley <dave@rotwang.co.uk>
2015-10-28 15:20:56 +0000

    tidy intelhex code

--- intelhex.py
+++ intelhex.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python
 
-path = "../arduino/sketchbook/broker_talk/build-uno/broker_talk.hex"
+#
+#   Read IntelHex file and generate binary file.
+#
+#   see : https://en.wikipedia.org/wiki/Intel_HEX
+
+#
+#   Split ASCII Hex bytes into binary numbers.
 
 def hex_split(line):
     for i in range(0, len(line), 2):
         yield(int(line[i:i+2], 16))
 
+#
+#   Wrap iterator and provide checksum calc.
+
 class CS:
     def __init__(self, iterator):
         self.cs = 0
         self.it = iterator
-    def next(self, add=True):
+    def next(self):
         data = self.it.next()
-        if add:
-            self.cs += data
+        self.cs += data
         return data
     def get(self):
-        cs = (~self.cs) & 0xFF
-        return (1 + cs) & 0xFF
+        # 2's complement
+        return (1 + ~self.cs) & 0xFF
+
+#
+#   Read IntelHex line and convert into :
+#   (record_type, addr, data).
 
 def parse(line):
     assert line[0] == ":", line
-    it = hex_split(line[1:])
-    it = CS(it)
+    hit = hex_split(line[1:])
+    it = CS(hit)
     size = it.next()
     addr = it.next()
     addr *= 256
@@ -31,7 +43,7 @@ def parse(line):
     data = []
     for i in range(size):
         data.append(it.next())
-    cs = it.next(False)
+    cs = hit.next()
     try:
         it.next()
         raise Exception("wrong length record")
@@ -45,21 +57,31 @@ def parse(line):
 #
 #
 
-of = file("/tmp/ihex.bin", "w")
+def convert(path, ofile, verbose=False):
+    for line in file(path):
+        record_type, addr, data = parse(line.strip())
+        if verbose:
+            print record_type, addr, data
+        if record_type == 0:
+            if verbose:
+                print "data ...", addr
+            ofile.seek(addr)
+            s = [ chr(c) for c in data ]
+            s = "".join(s)
+            ofile.write(s)
+        elif record_type == 1:
+            if verbose:
+                print "EOF"
+            ofile.close()
+        else:
+            raise Exception("Unknown record type " + line)
 
-for line in file(path):
-    record_type, addr, data = parse(line.strip())
-    print record_type, addr, data
-    if record_type == 0:
-        print "data ...", addr
-        of.seek(addr)
-        s = [ chr(c) for c in data ]
-        s = "".join(s)
-        of.write(s)
-    elif record_type == 1:
-        print "EOF"
-        of.close()
-    else:
-        raise Exception("Unknown record type " + line)
+#
+#
+
+if __name__ == "__main__":
+    path = "../arduino/sketchbook/radio_relay/build-uno/radio_relay.hex"
+    of = file("/tmp/ihex.bin", "w")
+    convert(path, of, True)
 
 # FIN
