1e525a38eba636872baa7072b5a75317a6bc6352
Dave Berkeley <dave@rotwang.co.uk>
2017-08-01 13:54:03 +0100

    api change in lua library

--- esp8266/lua/tx.lua
+++ esp8266/lua/tx.lua
@@ -1,6 +1,6 @@
 
 function tx(req)
-    conn=net.createConnection(net.TCP, false)
+    conn=net.createConnection(net.TCP, 0)
     conn:on("receive", function(conn, pl) print("got reply") end)
     conn:connect(80,"192.168.0.56")
     conn:send("GET /" .. req .. " HTTP/1.1\r\nHost: esp8266\r\n\r\n")
