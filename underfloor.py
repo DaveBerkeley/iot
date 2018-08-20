1b100e50d5d7e79f76980152cd4194b6e4e16e30
Dave Berkeley <via@rotwang.co.uk>
2018-08-20 20:54:00 +0100

    wip: added underfloor.py

---
+++ underfloor.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python -u
+
+import os
+import time
+import threading
+
+# http://pyserial.sourceforge.net/
+import serial
+
+import broker
+
+#
+#
+
+lock = threading.Lock()
+
+def log(*args):
+    with lock:
+        print time.strftime("%y/%m/%d %H:%M:%S :"), 
+        for arg in args:
+            print arg,
+        print
+
+#
+#
+
+def init_serial(path):
+    log("open serial '%s'" % path)
+    s = serial.Serial(path, baudrate=9600, timeout=1, rtscts=True)
+    log("serial opened", path)
+    return s
+
+#
+#
+
+class Underfloor:
+
+    def __init__(self, path):
+        self.dead = False
+        self.path = path
+        self.s = init_serial(self.path)
+
+    def monitor(self):
+        while not self.dead:
+            try:
+                line = self.s.readline()
+            except Exception, ex:
+                log("exception", str(ex))
+                time.sleep(10)
+                self.s = init_serial(self.path)
+                continue
+
+            if not line:
+                continue
+
+            log(line.strip())
+
+    def on_fan(self, x):
+        log("on_fan", x.payload)
+        # validate!
+
+    def on_pump(self, x):
+        log("on_pump", x.payload)
+        # validate!
+
+#
+#
+
+if __name__ == "__main__":
+    mqtt = broker.Broker("underfloor" + str(os.getpid()), server="mosquitto")
+
+    def wrap(fn):
+        def f(line):
+            try:
+                fn(line)
+            except Exception as ex:
+                log("Exception", str(fn), str(ex))
+        return f
+
+    usb = Underfloor("/dev/ttyUSB0")
+
+    mqtt.subscribe("home/fan/0", wrap(usb.on_fan))
+    mqtt.subscribe("home/pump/0", wrap(usb.on_pump))
+
+    try:
+        mqtt.start()
+        usb.monitor()
+    except KeyboardInterrupt:
+        pass
+    except Exception as ex:
+        log("exception", str(ex))
+
+    mqtt.stop()
+    mqtt.join()
+
+# FIN
