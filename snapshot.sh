3fbbd4e55f22c8890be912c2b90683913bf19348
Dave Berkeley <klatu@rotwang.co.uk>
2018-08-25 22:32:08 +0100

    retain mqtt message for snapshot camera/lamp power

--- snapshot.sh
+++ snapshot.sh
@@ -9,8 +9,8 @@ TOPIC_LAMP="home/usb/0/1"
 MQTT="mosquitto"
 
 # Turn on the camera and the lamp
-echo "S" | mosquitto_pub -h $MQTT -t $TOPIC_CAM -l
-echo "S" | mosquitto_pub -h $MQTT -t $TOPIC_LAMP -l
+echo "S" | mosquitto_pub -h $MQTT -t $TOPIC_CAM -l -r
+echo "S" | mosquitto_pub -h $MQTT -t $TOPIC_LAMP -l -r
 
 # wait for drivers to start, AGC to stabilise ..
 sleep 4
@@ -26,8 +26,11 @@ fi
 # Take the photo (camera is upside down, so flip)
 fswebcam --skip 25 --flip h --flip v --device $DEV $P
 
+# copy it somewhere we can view it on wiki
+cp $P "/usr/local/data/DIGICAM/$NAME.jpg"
+
 # turn the lamp off. Leave the camera
-#echo "R" | mosquitto_pub -h $MQTT -t $TOPIC_CAM -l
-echo "R" | mosquitto_pub -h $MQTT -t $TOPIC_LAMP -l
+#echo "R" | mosquitto_pub -h $MQTT -t $TOPIC_CAM -l -r
+echo "R" | mosquitto_pub -h $MQTT -t $TOPIC_LAMP -l -r
 
 # FIN
