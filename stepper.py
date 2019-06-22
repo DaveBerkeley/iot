7032f15746e4360689a599caae281cde274be543
Dave Berkeley <via@rotwang.co.uk>
2019-06-22 07:09:50 +0100

    wip: stepper control

--- stepper.py
+++ stepper.py
@@ -16,44 +16,33 @@ def log(*args):
         print arg,
     print
 
-dev = sys.argv[1]
-
-"""
-ser = serial.Serial(dev, 9600, timeout=1)
-
-time.sleep(1);
-ser.write("\r\n")
-time.sleep(1);
-
-ser.write("G1000\r\n")
-time.sleep(5);
-
-ser.write("G2000\r\n")
-
-time.sleep(5)
-"""
+#
+#
 
 def init_serial(dev):
     return serial.Serial(dev, 9600, timeout=1)
+
+def command(text):
+    global s
+    log("cmd", text)
+    s.write(text + "\r\n")
 
 def on_mqtt(client, x, msg):
     global s
     try:
         if s is None:
             s = init_serial()
-        log(msg.payload)
+        #log(msg.payload)
         data = json.loads(msg.payload)
         log(msg.topic, data)
         cmd = data["cmd"]
-        value = data.get("value", "")
-        s.write(str(cmd + str(int(value)) + "\r\n"))
-        #args = data.get("args", [])
-        #dev = data["dev"] - base_dev
-        #if 0 <= dev <= 3:
-        #    fn(dev, *args)
+        value = data.get("value", "0")
+        text = str(cmd + str(int(value)))
+        command(text)
+        log("done")
     except (serial.serialutil.SerialException, OSError), ex:
         # shut down the serial port and reconnect
-        log(str(ex))
+        log(str(ex), msg.payload)
         s = None
 
 #
@@ -64,6 +53,8 @@ if __name__ == "__main__":
     p.add_option("-s", "--serial", dest="serial", type="str", default='/dev/arduino')
     p.add_option("-m", "--mqtt-server", dest="mqtt", default="mosquitto")
     p.add_option("-d", "--dev", dest="dev", type="int", default=0)
+    p.add_option("-r", "--range", dest="range", type="int", default=3500)
+    p.add_option("-t", "--topic", dest="topic", default="home/stepper/0")
 
     opts, args = p.parse_args()
 
@@ -71,12 +62,20 @@ if __name__ == "__main__":
     base_dev = opts.dev
     s = init_serial(serial_dev)
 
+    # flush the stepper's command buffer
+    time.sleep(1);
+    command("")
+    time.sleep(1);
+    command("S%d" % opts.range);
+    time.sleep(1);
+
     name = time.strftime("stepper_%Y%m%d%H%M")
     mqtt = paho.Client(name)
     mqtt.connect(opts.mqtt)
 
     mqtt.on_message = on_mqtt
-    mqtt.subscribe("home/stepper/0")
+    log("subscribe", opts.topic)
+    mqtt.subscribe(opts.topic)
 
     log("start MQTT client '%s'" % name)
 
